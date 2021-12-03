95e67d714d2cbd53f2e8f50f707c38997b036632
adevgg <adevgg9@gmail.com>
2021-12-03 15:05:48 +0800

    feat: hello compass web

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Center } from "./components/basic";
+
+function App() {
+  return (
+    <Center sx={{ marginTop: 64 }}>
+      <Typography variant={"h1"}>Compass Web</Typography>
+    </Center>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { store } from "./redux/store";
+
+const darkModeTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={darkModeTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
