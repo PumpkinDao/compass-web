f2212969c39e7c89194076f8e7cd598e731a6eb7
adevgg <adevgg9@gmail.com>
2022-02-23 23:56:01 +0800

    fix: fix style

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,8 +3,6 @@
 interface Window {
   ethereum?: {
     isMetaMask?: true;
-    on?: (...args: any[]) => void;
-    removeListener?: (...args: any[]) => void;
     autoRefreshOnNetworkChange?: boolean;
   };
   web3?: Record<string, unknown>;
