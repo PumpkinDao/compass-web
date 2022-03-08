fadf39775ec9508a1d0db09cdc8720b9c2e9acfb
adevgg <adevgg9@gmail.com>
2022-03-08 12:28:48 +0800

    feat: define some editor context typing

--- src/components/monaco-editor/index.tsx
+++ src/components/monaco-editor/index.tsx
@@ -1,7 +1,64 @@
-import Editor, { EditorProps } from "@monaco-editor/react";
+import Editor, { EditorProps, Monaco } from "@monaco-editor/react";
+import { useCallback } from "react";
 
-const MonacoEditor = ({ theme, ...rest }: EditorProps) => (
-  <Editor theme={theme || "vs-dark"} {...rest} />
-);
+const setupTypescriptEditor = (monaco: Monaco) => {
+  // validation settings
+  monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: false,
+  });
+
+  // compiler options
+  monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+    target: monaco.languages.typescript.ScriptTarget.ES6,
+    allowNonTsExtensions: true,
+  });
+
+  // extra libraries
+  const libGlobalTyping = `
+declare const cache: Record<string, string | number | boolean>;
+declare namespace evm {
+  function getProvider(
+    network:
+      | "ethereum"
+      | "ropsten"
+      | "bsc"
+      | "polygon"
+      | "ftm"
+      | "avax"
+      | "optimism"
+      | "arbitrum"
+      | "moonriver"
+  ): any;
+}
+declare const ethers: any;
+`;
+
+  const libGlobalUri = "ts:global.d.ts";
+  monaco.languages.typescript.javascriptDefaults.addExtraLib(
+    libGlobalTyping,
+    libGlobalUri
+  );
+  monaco.editor.createModel(
+    libGlobalTyping,
+    "typescript",
+    monaco.Uri.parse(libGlobalUri)
+  );
+};
+
+const MonacoEditor = (props: EditorProps) => {
+  const { theme, language, defaultLanguage } = props;
+
+  const onMount = useCallback(
+    (editor, monaco) => {
+      if (language === "typescript" || defaultLanguage === "typescript") {
+        setupTypescriptEditor(monaco);
+      }
+    },
+    [language, defaultLanguage]
+  );
+
+  return <Editor {...props} theme={theme || "vs-dark"} onMount={onMount} />;
+};
 
 export default MonacoEditor;
